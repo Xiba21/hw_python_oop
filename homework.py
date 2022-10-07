833028decffd5c623e50b080a12514d10375229b
Alexander <arozinkov2@gmail.com>
2022-10-07 10:51:06 +0300

    naming, err exception

--- homework.py
+++ homework.py
@@ -22,12 +22,6 @@ class Training:
     """Базовый класс тренировки."""
     M_IN_KM: int = 1000
     LEN_STEP: float = 0.65
-<<<<<<< HEAD
-=======
-    training_type: str = ''
-    COEFF_CALORIE_1: float = 0
-    COEFF_CALORIE_2: float = 0
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
     MINS_IN_HOUR: int = 60
 
     def __init__(self,
@@ -54,11 +48,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-<<<<<<< HEAD
         return InfoMessage(self.__class__.__name__,
-=======
-        return InfoMessage(self.training_type,
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
                            self.duration, self.get_distance(),
                            self.get_mean_speed(),
                            self.get_spent_calories())
@@ -67,7 +57,6 @@ class Training:
 class Running(Training):
     """Тренировка: бег."""
     LEN_STEP = 0.65
-<<<<<<< HEAD
     CALORIES_MEAN_SPEED_MULTIPLIER = 18
     CALORIES_MEAN_SPEED_SHIFT = 20
 
@@ -75,19 +64,6 @@ class Running(Training):
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
                 * self.get_mean_speed()
                 - self.CALORIES_MEAN_SPEED_SHIFT)
-=======
-    COEFF_CALORIE_1 = 18
-    COEFF_CALORIE_2 = 20
-
-    def __init__(self, action: int, duration: float, weight: float) -> None:
-        super().__init__(action, duration, weight)
-        self.training_type = 'Running'
-
-    def get_spent_calories(self) -> float:
-        return ((self.COEFF_CALORIE_1
-                * self.get_mean_speed()
-                - self.COEFF_CALORIE_2)
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
                 * self.weight / self.M_IN_KM
                 * self.duration
                 * self.MINS_IN_HOUR)
@@ -96,13 +72,8 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
     LEN_STEP = 0.65
-<<<<<<< HEAD
     CALORIES_WEIGHT_COEFF = 0.035
     CALORIES_MEAN_SPEED_COEFF = 0.029
-=======
-    COEFF_CALORIE_1 = 0.035
-    COEFF_CALORIE_2 = 0.029
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
 
     def __init__(self,
                  action: int,
@@ -113,19 +84,11 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-<<<<<<< HEAD
         return ((self.CALORIES_WEIGHT_COEFF
                 * self.weight
                 + (self.get_mean_speed()**2
                    // self.height)
                 * self.CALORIES_MEAN_SPEED_COEFF
-=======
-        return ((self.COEFF_CALORIE_1
-                * self.weight
-                + (self.get_mean_speed()**2
-                   // self.height)
-                * self.COEFF_CALORIE_2
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
                 * self.weight)
                 * self.duration
                 * self.MINS_IN_HOUR)
@@ -134,13 +97,8 @@ class SportsWalking(Training):
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP = 1.38
-<<<<<<< HEAD
     CALORIES_MEAN_SPEED_SHIFT = 1.1
     CALORIES_MEAN_SPEED_MULTIPLIER = 2
-=======
-    COEFF_CALORIE_1 = 1.1
-    COEFF_CALORIE_2 = 2
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
 
     def __init__(self,
                  action: int,
@@ -160,13 +118,8 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed()
-<<<<<<< HEAD
                 + self.CALORIES_MEAN_SPEED_SHIFT)
                 * self.CALORIES_MEAN_SPEED_MULTIPLIER
-=======
-                + self.COEFF_CALORIE_1)
-                * self.COEFF_CALORIE_2
->>>>>>> 8f91d9b6d50f3a121fe85c2cdf42453a8953c6a1
                 * self.weight)
 
 
